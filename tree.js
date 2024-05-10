522303cf46b5f38b2f45ed2b14a56d5cf54208a7
madaooftheblues <azlanrasool@gmail.com>
2024-05-10 16:56:46 +0500

    Implement function createBST

--- tree.js
+++ tree.js
@@ -1,8 +1,10 @@
+import Node from './node.js'
+
 function Tree(arr) {
     let root = buildTree(arr)
 
     function removeDuplicates(arr) {
-        return [...set(arr)]
+        return [...new Set(arr)]
     }
 
     function sortElements(arr) {
@@ -17,9 +19,38 @@ function Tree(arr) {
 
     function buildTree(arr) {
         const processed = processArray(arr)
-
         const root = createBST(processed, 0, processed.length - 1)
+        return root
+    }
+
+    function createBST(arr, start, end) {
+        if (start > end) return null
+
+        const mid = Math.floor((start + end) / 2)
+        const root = Node(arr[mid])
+
+        root.left = createBST(arr, start, mid - 1)
+        root.right = createBST(arr, mid + 1, end)
 
         return root
     }
+
+    function prettyPrint(node, prefix = '', isLeft = true) {
+        if (node === null) {
+            return
+        }
+        if (node.right !== null) {
+            prettyPrint(
+                node.right,
+                `${prefix}${isLeft ? '│   ' : '    '}`,
+                false,
+            )
+        }
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`)
+        if (node.left !== null) {
+            prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true)
+        }
+    }
+
+    return { prettyPrint, root }
 }
